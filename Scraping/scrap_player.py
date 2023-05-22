103724e846d966cc4774491c95a2c7f7914c283b
sina <sinaof1381@gmail.com>
2023-05-22 15:40:34 +0430

    link_of_player

---
+++ Scraping/scrap_player.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import time
+import re
+import numpy as np
+
+headers = {
+    'Accept-Language': 'en-US',
+    "User-Agent": "Chrome/112.0.5615.138"
+}
+
+#get links in feature
+response_link = requests.get('https://www.transfermarkt.com/chelsea-fc/startseite/verein/631?saison_id=2020', headers=headers)
+response_link.content.decode()
+soup_link = BeautifulSoup(response_link.content, 'html.parser')
+
+#link each player
+n = soup_link.select('.nowrap a')
+for i in range (0,len(n),2):
+    print('https://www.transfermarkt.com/' + n[i].get('href'))
