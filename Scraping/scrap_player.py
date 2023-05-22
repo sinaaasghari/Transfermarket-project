440aff87982df4c28e51fddefdb4033e5d45d246
sina <sinaof1381@gmail.com>
2023-05-22 19:42:25 +0430

    add_template_loop

--- Scraping/scrap_player.py
+++ Scraping/scrap_player.py
@@ -17,84 +17,98 @@ response_link = requests.get('https://www.transfermarkt.com/chelsea-fc/startseit
 response_link.content.decode()
 soup_link = BeautifulSoup(response_link.content, 'html.parser')
 
-#link each player
-n = soup_link.select('.nowrap a')
-for i in range (0,len(n),2):
-    print('https://www.transfermarkt.com' + n[i].get('href'))
+#change conditon of rule
+Players = []
+for i in range(100):
+    response_link = requests.get("link_of_json_arsalan", headers=headers)
+    response_link.content.decode()
+    soup_link = BeautifulSoup(response_link.content, 'html.parser')
+    n = soup_link.select('.nowrap a')
+    for i in range (0,len(n),2):
+        response_player = requests.get('https://www.transfermarkt.com' + n[i].get('href'), headers=headers)
+        response_player.content.decode()
+        soup_player = BeautifulSoup(response_player.content, 'html.parser')
+        Player = {}
+        try:
+            number = re.search(r'\d+$', n[i].get('href')).group()
+            Player['player_id'] = number
+        except:
+            Player['player_id'] = np.nan
+        
+        try:
+            Player['Name'] = n[i].text
+        except:
+            Player['Name'] = np.nan
+        
+        try:
+            Player['Main_position'] = soup_player.select('.detail-position__position')[0].text
+        except:
+            Player['Main_position'] = np.nan
+        
+        try:
+            nt = soup_player.select('.tm-player-market-value-development__current-value')[0].text
+            nt = nt.strip()
+            nt = nt.replace('€', '')
+            if nt[-1:] == 'm':
+                vlaue = float(nt[:-1]) * 1000000
+            else:
+                vlaue = float(nt[:-1]) * 1000
+
+            Player['Market_Value'] = vlaue
+        except:
+            Player['Market_Value'] = np.nan
 
 
-#name player
-n = soup_link.select('.nowrap a')
-for i in range (0,len(n),2):
-    print(n[i].text)
 
+            
+            text = soup_player.select('.info-table--right-space')[0].text
+            # Extracting Date of birth
+            dob_pattern = r"Date of birth:\n(.*?)\n"
+            dob_match = re.search(dob_pattern, text)
+            if dob_match:
+                date_of_birth = dob_match.group(1)
+            else:
+                date_of_birth = np.nan
 
-#primry key for each player
-n = soup_link.select('.nowrap a')
-for i in range (0,len(n),2):
-    number = re.search(r'\d+$', n[i].get('href')).group()
-    print(number)
+            # Extracting Age
+            age_pattern = r"Age:\n(.*?)\n"
+            age_match = re.search(age_pattern, text)
+            if age_match:
+                age = int(age_match.group(1))
+            else:
+                age = np.nan
 
-#link replce from first part
-response_link_player = requests.get('https://www.transfermarkt.com/daryl-janmaat/profil/spieler/60744', headers=headers)
-response_link_player.content.decode()
-soup_link_player = BeautifulSoup(response_link_player.content, 'html.parser')
+            # Extracting Foot
+            foot_pattern = r"Foot:\n(.*?)\n"
+            foot_match = re.search(foot_pattern, text)
+            if foot_match:
+                foot = foot_match.group(1)
+            else:
+                foot = np.nan
 
-#main position
-soup_link_player.select('.detail-position__position')[0].text
+            # Extracting Player agent
+            agent_pattern = r"Player agent:\n\n(.*?)\n\n"
+            agent_match = re.search(agent_pattern, text)
+            if agent_match:
+                player_agent = agent_match.group(1)
+            else:
+                player_agent = np.nan
 
-#market_value
-soup_link_player.select('.tm-player-market-value-development__current-value')[0].text
+            # Extracting Height
+            height_pattern = r"Height:\n(.*?)\xa0"
+            height_match = re.search(height_pattern, text)
+            if height_match:
+                height = height_match.group(1)
+            else:
+                height = np.nan
 
-#current_id_club
-#give_that_data_from another teammate
+            Player['Date_of_birth'] = date_of_birth
+            Player['Age'] = age
+            Player['Height'] = height
+            Player['Foot'] = foot
+            Player['Ajent'] = player_agent
+            Player['Current_club_ID']
 
-
-text = soup_link_player.select('.info-table--right-space')[0].text
-
-# Extracting Date of birth
-dob_pattern = r"Date of birth:\n(.*?)\n"
-dob_match = re.search(dob_pattern, text)
-if dob_match:
-    date_of_birth = dob_match.group(1)
-else:
-    date_of_birth = None
-
-# Extracting Age
-age_pattern = r"Age:\n(.*?)\n"
-age_match = re.search(age_pattern, text)
-if age_match:
-    age = age_match.group(1)
-else:
-    age = None
-
-# Extracting Foot
-foot_pattern = r"Foot:\n(.*?)\n"
-foot_match = re.search(foot_pattern, text)
-if foot_match:
-    foot = foot_match.group(1)
-else:
-    foot = None
-
-# Extracting Player agent
-agent_pattern = r"Player agent:\n\n(.*?)\n\n"
-agent_match = re.search(agent_pattern, text)
-if agent_match:
-    player_agent = agent_match.group(1)
-else:
-    player_agent = None
-
-# Extracting Height
-height_pattern = r"Height:\n(.*?)\xa0"
-height_match = re.search(height_pattern, text)
-if height_match:
-    height = height_match.group(1)
-else:
-    height = None
-
-print("Date of birth:", date_of_birth)
-print("Age:", age)
-print("Foot:", foot)
-print("Player agent:", player_agent)
-print("Height:", height)
+            Players.append(Player)
+            
 
